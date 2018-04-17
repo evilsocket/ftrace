0bc77ae83ffff1e04a135d34e7f040df70155e79
evilsocket <evilsocket@gmail.com>
2018-04-17 16:02:21 +0200

    misc: small fix or general refactoring i did not bother commenting

--- probe.go
+++ probe.go
@@ -57,24 +57,24 @@ func NewProbe(name string, syscall string, subEvents []string) *Probe {
 }
 
 // Enabled return true if this probe is enabled and running, otherwise false.
-func (this *Probe) Enabled() bool {
-	this.RLock()
-	defer this.RUnlock()
-	return this.enabled
+func (p *Probe) Enabled() bool {
+	p.RLock()
+	defer p.RUnlock()
+	return p.enabled
 }
 
 // Events returns a channel where FTRACE events will be written by this Probe worker routine.
-func (this *Probe) Events() <-chan Event {
-	return this.bus
+func (p *Probe) Events() <-chan Event {
+	return p.bus
 }
 
-func (this *Probe) selectEvent(event string) bool {
+func (p *Probe) selectEvent(event string) bool {
 	// main probe event
-	if strings.Contains(event, this.name) {
+	if strings.Contains(event, p.name) {
 		return true
 	}
 	// one of the sub events
-	for eventName := range this.events {
+	for eventName := range p.events {
 		if strings.Contains(event, eventName) {
 			return true
 		}
@@ -82,36 +82,36 @@ func (this *Probe) selectEvent(event string) bool {
 	return false
 }
 
-func (this *Probe) worker() {
+func (p *Probe) worker() {
 	// signal we're done when we exit
 	defer func() {
-		this.done <- true
+		p.done <- true
 	}()
 
-	for eventLine := range this.pipe {
+	for eventLine := range p.pipe {
 		// our parent go routine is telling us to quit
 		if eventLine == "<quit>" {
 			break
 		}
 
 		// check if we're interested in this event
-		if this.selectEvent(eventLine) {
+		if p.selectEvent(eventLine) {
 			// parse the raw event data
 			if event, err := parseEvent(eventLine); err != nil {
 				fmt.Printf("Error while parsing event: %s\n", err)
 			} else {
-				this.bus <- event
+				p.bus <- event
 			}
 		}
 	}
 }
 
 // Enable enables this probe and starts its async worker routine in order to read FTRACE events.
-func (this *Probe) Enable() (err error) {
-	this.Lock()
-	defer this.Unlock()
+func (p *Probe) Enable() (err error) {
+	p.Lock()
+	defer p.Unlock()
 
-	if this.enabled == true {
+	if p.enabled == true {
 		return nil
 	}
 
@@ -120,67 +120,67 @@ func (this *Probe) Enable() (err error) {
 	}
 
 	// enable all events
-	for eventName, eventFileName := range this.events {
+	for eventName, eventFileName := range p.events {
 		if err = writeFile(eventFileName, "1"); err != nil {
 			return fmt.Errorf("Error while enabling event %s: %s", eventName, err)
 		}
 	}
 
 	// create the custom kprobe consumer
-	if err = writeFile(systemProbesFile, this.descriptor); err != nil {
-		return fmt.Errorf("Error while enabling probe descriptor for %s: %s", this.name, err)
+	if err = writeFile(systemProbesFile, p.descriptor); err != nil {
+		return fmt.Errorf("Error while enabling probe descriptor for %s: %s", p.name, err)
 	}
 
 	// enable the probe
-	if err = writeFile(this.fileName, "1"); err != nil {
-		return fmt.Errorf("Error while enable probe %s: %s", this.name, err)
+	if err = writeFile(p.fileName, "1"); err != nil {
+		return fmt.Errorf("Error while enable probe %s: %s", p.name, err)
 	}
 
 	// create the handle to the pipe file
-	if this.pipe, err = asyncFileReader(eventsPipeFile); err != nil {
+	if p.pipe, err = asyncFileReader(eventsPipeFile); err != nil {
 		return fmt.Errorf("Error while opening %s: %s", eventsPipeFile, err)
 	}
 
-	this.enabled = true
+	p.enabled = true
 
 	// start the async worker that will read events from the
 	// pipe file and send them to the `bus` channel
-	go this.worker()
+	go p.worker()
 
 	return nil
 }
 
 // Disable disables this probe and stops its async worker.
-func (this *Probe) Disable() error {
-	this.Lock()
-	defer this.Unlock()
+func (p *Probe) Disable() error {
+	p.Lock()
+	defer p.Unlock()
 
-	if this.enabled == false {
+	if p.enabled == false {
 		return nil
 	}
 
 	// disable all events
-	for eventName, eventFileName := range this.events {
+	for eventName, eventFileName := range p.events {
 		if err := writeFile(eventFileName, "0"); err != nil {
 			return fmt.Errorf("Error while disabling event %s: %s", eventName, err)
 		}
 	}
 
 	// disable the probe itself
-	if err := writeFile(this.fileName, "0"); err != nil {
-		return fmt.Errorf("Error while disabling probe %s: %s", this.name, err)
+	if err := writeFile(p.fileName, "0"); err != nil {
+		return fmt.Errorf("Error while disabling probe %s: %s", p.name, err)
 	}
 
 	// remove the probe from the system
-	if err := appendFile(systemProbesFile, fmt.Sprintf("-:%s", this.name)); err != nil {
-		return fmt.Errorf("Error while removing the probe %s: %s", this.name, err)
+	if err := appendFile(systemProbesFile, fmt.Sprintf("-:%s", p.name)); err != nil {
+		return fmt.Errorf("Error while removing the probe %s: %s", p.name, err)
 	}
 
-	this.enabled = false
-	this.pipe <- "<quit>"
+	p.enabled = false
+	p.pipe <- "<quit>"
 
 	// wait for the worker to finish
-	<-this.done
+	<-p.done
 
 	return nil
 }
